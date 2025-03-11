5072c63a2729633b3b9b7f5dce978d899dabcd9e
Brenn515 <bdc69c@umsystem.edu>
2025-03-11 22:29:51 +0000

    Added to Astar

--- AirToGroundSearch/src/algorithms/a_star.py
+++ AirToGroundSearch/src/algorithms/a_star.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import heapq
 from typing import Tuple
 from enum import Enum
 from datetime import time, timedelta
@@ -11,6 +12,16 @@ def h(x, y, g_x, g_y):
     # returns the straight-line distance between the goal and current node
     return math.sqrt((g_x - x) ** 2 + (g_y - y) ** 2)
 
+def is_valid(range_row, range_col):
+    return range_row >= 0 and range_col >= 0
+
+def is_unblocked(grid, row, col):
+    return grid[row][col] == 0
+
+def is_destination(row, col, goal_coords):
+    return row == goal_coords[0] and col == goal_coords[1]
+
+
 
 def a_star_search(
     grid: pd.DataFrame,
@@ -22,13 +33,41 @@ def a_star_search(
     start_row, start_col = start_index
     range_row, range_col = radar_range
     goal_row, goal_col = goal_coords
+    found_destination = False
 
     if direction > 1:
         range_row, range_col = range_col, range_row
 
+    if not is_unblocked(grid, start_row, start_col) or not is_unblocked(grid, goal_row, goal_col):
+        print("Start or goal is blocked")
+        return
+
     open_set: list[Tuple[int, int, int]] = [(start_row, start_col, direction)]
 
     came_from: list[Tuple[int, int, int]] = []
+
+    while open_set is not None:
+        current = heapq.heappop(open_set)
+        current_row, current_col, current_dir = current
+
+        if current_row == goal_row and current_col == goal_col:
+            found_destination = True
+            break
+
+        for i in range(4):
+            new_row, new_col = current_row, current_col
+            if i == 0: #N
+                new_row += 1
+            elif i == 1: #S
+                new_row -= 1
+            elif i == 2: #E
+                new_col += 1
+            else: #W
+                new_col -= 1
+
+            if is_valid(new_row, new_col) and is_unblocked(grid, new_row, new_col) and not came_from[new_row][new_col]:
+                heapq.heappush(open_set, (new_row, new_col, i))
+                came_from.append((new_row, new_col, i))
 
     g_score = defaultdict(lambda: math.inf)
     g_score[start_index] = 0
