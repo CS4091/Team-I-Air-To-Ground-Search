5aa0be6e91693b1e9bb532162a55fb69d0bc9e9b
Will Weidler <weidlerwill@gmail.com>
2025-03-10 19:09:43 -0500

    added gitkeeps to keep output folders

--- AirToGroundSearch/obj/Debug/net8.0/AirToGroundSearch.AssemblyInfo.cs
+++ AirToGroundSearch/obj/Debug/net8.0/AirToGroundSearch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirToGroundSearch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a394377711c4cbadade154faaeeb3ce3f2388105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad042d08fd722df87ae4984afd6dfc42377eb9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirToGroundSearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirToGroundSearch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
