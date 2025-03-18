7c65c5d63ff4ac6d48a3a8acae3a7f775e2958b0
Will Weidler <weidlerwill@gmail.com>
2025-03-17 20:20:26 -0500

    refined importing by clearing old files when a new one is uploaded

--- AirToGroundSearch/obj/Debug/net8.0/AirToGroundSearch.AssemblyInfo.cs
+++ AirToGroundSearch/obj/Debug/net8.0/AirToGroundSearch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirToGroundSearch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad042d08fd722df87ae4984afd6dfc42377eb9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c460ef4dae76c57d8d05f31edc4f90864be7880a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirToGroundSearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirToGroundSearch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
