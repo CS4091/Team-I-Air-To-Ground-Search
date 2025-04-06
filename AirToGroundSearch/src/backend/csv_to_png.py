aecf37da87aba92e3d7280e88076a3126be2265b
Bailey S <baileyps03@gmail.com>
2025-04-06 17:23:01 +0000

    name additions

--- AirToGroundSearch/src/backend/csv_to_png.py
+++ AirToGroundSearch/src/backend/csv_to_png.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import sys
 
-def display_grid(grid):
+def display_grid(grid, name):
     cmap = mcolors.ListedColormap(["black", "white", "red", "green", "purple"])
     bounds = [0, 1, 2, 3, 4]
     norm = mcolors.BoundaryNorm(bounds, cmap.N)
     plt.axis("off")
-    plt.savefig("./wwwroot/outputs/GeneratedGrid/grid_world.png")
+    plt.savefig("/workspaces/Team-I-Air-To-Ground-Search/AirToGroundSearch/wwwroot/outputs/GeneratedGrid/" + name)
 
 if __name__ == "__main__":
     filepath = sys.argv[1]
+    name = sys.argv[2]
     grid = np.genfromtxt(filepath, delimiter=',')
-    display_grid(grid)
+    display_grid(grid, name)
 
 
 
