baf5f3e7c963c3af0c85ade3a093c46b4eac9109
Brenn515 <bdc69c@umsystem.edu>
2025-04-08 19:51:17 +0000

    Merge prep

--- AirToGroundSearch/src/algorithms/d_search.py
+++ AirToGroundSearch/src/algorithms/d_search.py
@@ -3,12 +3,11 @@ import heapq
 from typing import Tuple
 from ..external.drone import Drone, update_scanned_grid, find_start_coordinate
 
-
 def dijkstra_scan(grid: np.ndarray, fuel: int, scanned_grid: np.ndarray, drone: Drone):
     rows, cols = grid.shape
-    print(f"Start Position: {drone.row} {drone.col}")
+    #print(f"Start Position: {drone.row} {drone.col}")
     start = (drone.row, drone.col)
-    print(drone.row, drone)
+    #print(drone.row, drone)
     costs = np.full((rows, cols), np.inf)
     visited = np.zeros_like(grid, dtype=bool)
     queue = []
@@ -19,7 +18,7 @@ def dijkstra_scan(grid: np.ndarray, fuel: int, scanned_grid: np.ndarray, drone: 
 
     while queue and moves < fuel:
         current_cost, (current_row, current_col), current_direction = heapq.heappop(queue)
-        print(current_direction)
+        #print(current_direction)
 
         if visited[current_row, current_col]:
             continue
@@ -39,7 +38,6 @@ def dijkstra_scan(grid: np.ndarray, fuel: int, scanned_grid: np.ndarray, drone: 
         right = drone.direction
         drone.turn_left()
         allowed_directions = [forward, left, right]
-
 
         for direction in allowed_directions:
             original_row, original_col = drone.row, drone.col
@@ -65,18 +63,16 @@ def dijkstra_scan(grid: np.ndarray, fuel: int, scanned_grid: np.ndarray, drone: 
                 if new_cost < costs[neighbor_row, neighbor_col]:
                     costs[neighbor_row, neighbor_col] = new_cost
                     heapq.heappush(queue, (new_cost, (neighbor_row, neighbor_col), direction))
-                    print(f"Adding to queue: ({neighbor_row}, {neighbor_col}), Facing: {direction}, Cost: {new_cost}")
+                    #print(f"Adding to queue: ({neighbor_row}, {neighbor_col}), Facing: {direction}, Cost: {new_cost}")
 
             drone.row, drone.col = original_row, original_col
             drone.direction = original_direction
             scanned_grid[previous_row, previous_col] = 2
             scanned_grid[drone.row, drone.col] = 3
         
-        print(previous_row, previous_col)
-
-
-        print(f"Scanned grid:\n{scanned_grid}")
-        print(f"Queue state: {queue}")
+        #print(previous_row, previous_col)
+        #print(f"Scanned grid:\n{scanned_grid}")
+        #print(f"Queue state: {queue}")
 
     return scanned_grid
 
@@ -94,4 +90,4 @@ if __name__ == "__main__":
     print("Final scanned grid:")
     print(scanned_grid)
 
-#TODO Add 2x3 scanner, detect obstacles and avoid, improve logic behind path selection (Chosing the path with the most land to scan)
+#TODO detect obstacles and avoid, improve logic behind path selection (Chosing the path with the most land to scan), prevent diagonal movement, make drone.py detect walls
